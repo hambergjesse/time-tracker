f1b6a3c35df2bb5f9b7c95805653fe6e3b45b15d
Jesse Hamberg <hambergjesse@gmail.com>
2022-08-30 12:01:56 +0300

    foundation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import React from "react";
+import "./scss/index.scss";
 
 const App = () => {
   const [data, setData] = useState(null);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,35 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// connect to  mongodb
+async function main() {
+  const uri =
+    "mongodb+srv://hambergjesse:123@cluster0.osqpkfc.mongodb.net/test?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+// fetch databases from mongodb
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+main().catch(console.error);
+
+//
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
